abcf37efdf0b41a9737e246117afbc28198eda97
gazgiz <jlynnmail@gmail.com>
2022-11-28 14:21:42 -0600

    added additional skeleton files for routes

---
+++ src/routes/SelectCompany.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SelectCompany = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SelectCompany
---
+++ src/routes/UsersRegistry.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UsersRegistry = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UsersRegistry
