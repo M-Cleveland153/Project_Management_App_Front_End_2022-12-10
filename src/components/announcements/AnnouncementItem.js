ad82e12e18ae1e1c3f4a97ee505bea38d227964c
M-Cleveland153 <m999cleveland@gmail.com>
2022-11-28 22:17:33 -0600

    ready and preparing to merge announcements branch into Michael

---
+++ src/components/announcements/AnnouncementItem.js
@@ -0,0 +1,18 @@
+import Card from './Card';
+import classes from './Announcements.module.css'
+
+function AnnouncementItem(props){
+    return (
+        <li className={classes.li}>
+            <Card>
+                <p className={classes.user}>{props.user}</p>
+                <br/>
+                <p className={classes.date}>{props.date}</p>
+                
+                <p className={classes.message}>{props.message}</p>
+            </Card>
+        </li>
+    )
+}
+
+export default AnnouncementItem;
--- src/routes/Announcements.js
+++ src/routes/Announcements.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import AnnouncementList from "../components/announcements/AnnouncementList";
+import classes from "../components/announcements/Announcements.module.css";
 
 const Announcements = () => {
+  const loadedAnnouncements = [
+    {
+      id: 123456,
+      date: "2022-11-28",
+      title: "announcement title #1",
+      message:
+        "This is my first announcement  #1. Listen up I have an announcement to make. Listen up I have an announcement to make. Listen up I have an announcement to make. Listen up I have an announcement to make. Listen up I have an announcement to make. Listen up I have an announcement to make",
+      company: 987654,
+      user: "User Name",
+    },
+    {
+      id: 123457,
+      date: "2022-11-28",
+      title: "announcement title #2",
+      message:
+        "This is my second announcement #2. Listen up I have an announcement to make. Listen up I have an announcement to make. Listen up I have an announcement to make. Listen up I have an announcement to make. Listen up I have an announcement to make. Listen up I have an announcement to make.",
+      company: 987654,
+      user: "User Name",
+    },
+    {
+      id: 123458,
+      date: "2022-11-28",
+      title: "announcement title #3",
+      message:
+        "This is my third announcement #3. Listen up I have an announcement to make. Listen up I have an announcement to make. Listen up I have an announcement to make. Listen up I have an announcement to make. Listen up I have an announcement to make. Listen up I have an announcement to make.",
+      company: 987654,
+      user: "User Name",
+    },
+  ];
   return (
-    <div>
-      Announcements
-    </div>
-  )
-}
+    <section>
+      <h1 className={classes.h1}>Announcements</h1>
+      <div className={classes.actions}>
+        <button >New</button>
+      </div>
+      <hr className={classes.hr}></hr>
+      <AnnouncementList announcements={loadedAnnouncements} />
+    </section>
+  );
+};
 
-export default Announcements
+export default Announcements;
