1363dd31a1dc36d8d72db78d1b63bcc2e24494c6
Guy-Evens Gourdet <guygourdet7@gmail.com>
2022-11-28 23:49:29 -0500

    Added projects link to homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,35 @@
-import React, { useRef } from "react"
-import { FaBars } from "react-icons/fa"
-import "../components/NavbarStyles.css"
+import React, { useRef } from "react";
+import { FaBars } from "react-icons/fa";
+import "../components/NavbarStyles.css";
 
 const Navbar = () => {
-    const navRef = useRef()
+  const navRef = useRef();
 
-    const showNavbar = () =>{
-        navRef.current.classList.toggle("responsive_nav")
-    }
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-    return(
-        <header>
-            <div className="navbar-left">
-               <h3>Logo</h3> 
-               <p>Acting as admin</p>
-            </div>
-            <nav ref={navRef}>
-                <a href="/">Home</a>
-                <a href="/">Teams</a>
-                <a href="/">User</a>
-                <a href="/">Logout</a>
-                <button className="nav-btn nav-close-btn" onClick={showNavbar}><FaBars/></button>
-            </nav>
-            <button className="nav-btn" onClick={showNavbar}><FaBars/></button>
-        </header>
-    )
-}
+  return (
+    <header>
+      <div className="navbar-left">
+        <h3>Logo</h3>
+        <p>Acting as admin</p>
+      </div>
+      <nav ref={navRef}>
+        <a href="/">Home</a>
+        <a href="/">Teams</a>
+        <a href="/">User</a>
+        <a href="/">Projects</a>
+        <a href="/">Logout</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaBars />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
