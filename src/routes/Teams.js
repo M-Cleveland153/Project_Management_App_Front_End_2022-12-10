bc6ba3eec81584a60960fa0cc881f6c5b9eb15c2
Guy-Evens Gourdet <guygourdet7@gmail.com>
2022-11-29 12:11:21 -0500

    test

--- src/routes/Teams.js
+++ src/routes/Teams.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from "react";
+//import Navbar from "../components/Navbar";
 
 const Teams = () => {
-  return (
-    <div>
-      teams
-    </div>
-  )
-}
+  
+  
+  return <div>teams</div>;
+};
 
-export default Teams
+export default Teams;
