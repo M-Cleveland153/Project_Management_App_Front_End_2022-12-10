f781b00980d8e1365d69124e98df19fa3425725f
gazgiz <jlynnmail@gmail.com>
2022-12-01 13:18:14 -0600

    added GET request and response mapping to company selection

--- src/routes/SelectCompany.js
+++ src/routes/SelectCompany.js
@@ -1,20 +1,49 @@
 import React from "react";
+
 import "../components/selectCompany/SelectCompanyStyle.css";
 
+import useAxios from "../services/useAxios";
+
 const SelectCompany = () => {
+
+  const { response, loading, error } = useAxios({
+
+    method: 'get',
+    url: 'companies'
+  })
+
   return (
     <div id="select-company-container">
+      {console.log(response)}
       <h1 id="select-company-h1">Select Company</h1>
-      <select id="select-company-menu">
-        <option value="" disabled selected hidden>
-          Pick an option
-        </option>
-        <option value="fedEx">FedEx</option>
+      
+        {/* <option value="fedEx">FedEx</option>
         <option value="cookSystems">Cook Systems</option>
-        <option value="google">Google</option>
-      </select>
+        <option value="google">Google</option> */}
+      {loading ? ( <p>loading...</p>) : 
+       (
+        <div>
+            {error && (
+                <div>
+                    <p>{error.message}</p>
+                </div>
+            )}
+            <select id="select-company-menu">
+                <option value="" disabled selected hidden>
+                  Pick an option
+                </option>
+            {response.map((c) => (
+                <option key="company" value={c.company}>
+                  {c.company}
+                </option>
+            ))}
+            </select>
+        </div>
+      )}
     </div>
   );
 };
 
 export default SelectCompany;
+
+
